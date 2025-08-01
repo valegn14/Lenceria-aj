9b89d487eb97f23bd3c58f1213a101af5a86d16a
jhonniersanchez13 <jhonnierasanchez13@gmail.com>
2025-08-01 16:25:37 -0500

    Update visitanos.jsx

--- src/pages/visitanos.jsx
+++ src/pages/visitanos.jsx
@@ -31,7 +31,7 @@ function Mapa() {
   }, []);
 
   return (
-    <div>
+    <div className="p-5">
       {/* Este div es donde Google Maps pintará el mapa. */}
       <div id="map" style={{ height: "400px", width: "100%", borderRadius: "8px" }}></div>
     </div>
@@ -40,8 +40,8 @@ function Mapa() {
 
 const Visitanos = () => {
   return (
-    <div className='p-10 min-h-screen bg-gray-100 flex items-center justify-center'>
-      <div className='bg-white p-10 rounded-2xl shadow-2xl max-w-xl w-full text-center'>
+    <div className='mt-2 p-5 min-h-screen bg-gray-100 justify-center'>
+      <div className='bg-white p-5 rounded-2xl shadow-2xl w-full text-center'>
         <h1 className='text-3xl font-bold text-gray-800 mb-6'>Información de Contacto</h1>
         <p className='text-lg text-gray-700 mb-4'>
           <span className="font-semibold">Ubicación:</span> Manizales, Colombia
